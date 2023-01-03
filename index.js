f343c783d028c9700edfae3a2de518cb2cc3e6ac
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-03 18:17:22 +0300

    validate name

--- index.js
+++ index.js
@@ -2,73 +2,64 @@ console.log("linked")
 
 const form = document.querySelector("#card-form")
 
-
-const input = document.querySelectorAll('input')
-
-
-input.forEach(input =>  {
-    if (input.checked) {
-        console.log(input.value)
-    } else {
-        input.classList.add('inputcolor')
-    }
-    
-})
-
-console.log(input)
-
-
+//  submit form
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+   e.preventDefault()
 
-    renderCard()
-   renderSuccessMessage()
+// Get the credit card number, expiration date, CVC, and cardholder from the form
+   const cardName = document.getElementById("cardholdername").value
+   const cardNumber =  document.getElementById("cardnumber").value
+   const cardExpiryMonth = document.getElementById("experationdatemonth").value
+   const cardExpiryYear = document.getElementById("experationdateyear").value
+   const cardCVC = document.getElementById("cvcnumber").value
 
+   validateCardholder(cardName)
+
+  renderOnCard(cardName, cardNumber, cardExpiryMonth, cardExpiryYear, cardCVC)
 })
 
-
-// show success message after submit
-
-function renderSuccessMessage() {
-   const successMessage = document.querySelector(".success-message")
-   successMessage.innerHTML = `
-                                <img src="./images/icon-complete.svg" alt = "tick-icon">
-                                <p>We've added your card details
-                                    <button> continue</button>
-                                    </p>
-                                `
-   successMessage.style.display = "block"
-   successMessage.className.add(".success-message")
-   form.style.display = 'none'
-}
-
+function validateCardholder(cardName){
+    const errorMessage = document.querySelector('.cardNameerror');
+    if (cardName == ''){
+        errorMessage.textContent = "Error: name cannot be empty";
+    } 
+  }
 
 
 
 // render on card onsumbit
-
-function renderCard (){
-    const cardName = document.getElementsByTagName("input")[0].value
-    console.log(cardName)
+function renderOnCard(cardName, cardNumber, cardExpiryMonth, cardExpiryYear, cardCVC) {
+   
     const cardname = document.querySelector(".card-name")
-    console.log(cardname)
     cardname.innerHTML = cardName.toUpperCase()
+    console.log(cardname)
 
-    const cardNumber =  document.getElementsByTagName("input")[1].value
-    console.log(cardNumber)
     const cardnumber = document.querySelector(".card-number")
     cardnumber.innerHTML = cardNumber
-    cardnumber.classList.add(".spanNum")
+   
 
-    const cardExpiryMonth = document.getElementsByTagName("input")[2].value
     const cardexpirymonth = document.querySelector("#exp-month")
     cardexpirymonth.innerHTML = cardExpiryMonth
 
-    const cardExpiryYear = document.getElementsByTagName("input")[3].value
     const cardexpiryyear = document.querySelector("#exp-year")
     cardexpiryyear.innerHTML = cardExpiryYear
 
-    const cardCVC = document.getElementsByTagName("input")[4].value
     const cardcvc = document.querySelector("#cvc-num")
     cardcvc.innerHTML = cardCVC
 } 
+
+
+
+// show success message after submit
+function renderSuccessMessage() {
+    const successMessage = document.querySelector(".success-message")
+    successMessage.innerHTML = `
+                                 <img src="./images/icon-complete.svg" alt = "tick-icon">
+                                 <p>We've added your card details
+                                     <button> continue</button>
+                                     </p>
+                                 `
+    successMessage.style.display = "block"
+    successMessage.className.add(".success-message")
+    form.style.display = 'none'
+ }
