a9ccce2c6c79693225dff518a7af3e554496c957
abdishukri <abdishukrimohamed105@gmail.com>
2023-01-03 03:39:24 +0300

    successfully rendered details on card

--- index.js
+++ index.js
@@ -1,1 +1,60 @@
 console.log("linked")
+
+const form = document.querySelector("#card-form")
+
+
+const input = document.querySelectorAll('input')
+
+
+input.forEach(input =>  {
+    if (input.checked) {
+        console.log(input.value)
+    } else {
+        input.classList.add('inputcolor')
+    }
+    
+})
+
+console.log(input)
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    renderCard()
+
+
+})
+
+
+
+
+
+
+// render on card onsumbit
+
+function renderCard (){
+    const cardName = document.getElementsByTagName("input")[0].value
+    console.log(cardName)
+    const cardname = document.querySelector(".card-name")
+    console.log(cardname)
+    cardname.innerHTML = cardName.toUpperCase()
+
+    const cardNumber =  document.getElementsByTagName("input")[1].value
+    console.log(cardNumber)
+    const cardnumber = document.querySelector(".card-number")
+    cardnumber.innerHTML = cardNumber
+    cardnumber.classList.add(".spanNum")
+
+    const cardExpiryMonth = document.getElementsByTagName("input")[2].value
+    const cardexpirymonth = document.querySelector("#exp-month")
+    cardexpirymonth.innerHTML = cardExpiryMonth
+
+    const cardExpiryYear = document.getElementsByTagName("input")[3].value
+    const cardexpiryyear = document.querySelector("#exp-year")
+    cardexpiryyear.innerHTML = cardExpiryYear
+
+    const cardCVC = document.getElementsByTagName("input")[4].value
+    const cardcvc = document.querySelector("#cvc-num")
+    cardcvc.innerHTML = cardCVC
+} 
